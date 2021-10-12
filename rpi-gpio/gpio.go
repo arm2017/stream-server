75bee61b9dd56e259f20431fa63373e92d34df5a
Jeeraphan <arm2017@gmail.com>
2021-10-13 02:22:55 +0700

    pin

--- rpi-gpio/gpio.go
+++ rpi-gpio/gpio.go
@@ -23,16 +23,16 @@ func Setup() *GpioBoard {
 	defer rpio.Close()
 	g := GpioBoard{}
 	fmt.Printf("===>%T\n", rpio.Pin(4))
-	g.N1 = rpio.Pin(4) //GPIO7
+	g.N1 = rpio.Pin(23) //GPIO23
 	g.N1.Output()
 
-	g.N2 = rpio.Pin(17) //GPIO0
+	g.N2 = rpio.Pin(17) //GPIO17
 	g.N2.Output()
 
-	g.N3 = rpio.Pin(27) //GPIO2
+	g.N3 = rpio.Pin(27) //GPIO27
 	g.N3.Output()
 
-	g.N4 = rpio.Pin(22) //GPIO3
+	g.N4 = rpio.Pin(22) //GPIO22
 	g.N4.Output()
 
 	g.Clear()
