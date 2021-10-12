21003587755b59ae6ad10e30d17dc58916f8f7f3
Jeeraphan <arm2017@gmail.com>
2021-10-13 00:44:32 +0700

    host ip

--- move-register/move-register-client.go
+++ move-register/move-register-client.go
@@ -13,8 +13,8 @@ import (
 )
 
 const (
-	// address = "localhost:50051"
-	address = "0.tcp.ap.ngrok.io:18664"
+	address = "172.20.10.14:50051"
+	// address = "0.tcp.ap.ngrok.io:18664"
 )
 
 func MoveRegisterToServer(streamClient *api.StreamCameServiceClient) {
